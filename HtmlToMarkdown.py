c16c7fcff153a6d27c5dd7d95a36a9d71067e74f
olusegvn <41540079+olusegvn@users.noreply.github.com>
2019-02-02 05:41:46 -0800

    Update HtmlToMarkdown.py

--- HtmlToMarkdown.py
+++ HtmlToMarkdown.py
@@ -11,36 +11,33 @@ import subprocess
 print("_"*30 + "Convert HTML to Markdown" + "_"*30)
 
 
-
 #declare substitution array 
-sub_array = [["<p>", "\n"], ["</p>", "\n"], ["<br>", "\n"],
+sub_array = [["<head>", ""], ["<html>", ""],["</head>", ""],
+            ["</html>", ""], ["<body>", ""], ["</body>", ""],
+            ["<p>", "\n"], ["</p>", "\n"], ["<br>", "\n"],
             ["<strong><em>","***"], ["</em></strong>", "***"],
             ["<strong>", "**"], ["</strong>", "**"],["<ul>", ""],            
             ["<em>", "*"], ["</em>", "*"],["<li>","\n- "],
             ["</li>","\n"],["<code>","``"],["</code>", "``"]    
             ]
 
-
-
-
-#append header files (eg h1) to array
 i=1
+#append header files (eg h1) to array
 for i in range(11):
     header = "<h"+ str(i) +">"
-    htag = "#"*i
+    htag = "\n\n" + "#"*i
     sub_array.append([header, htag])
 
 
 #append end of header tags
 for i in range(11):
     header = "</h"+ str(i) +">"
-    htag = "#"*i
     sub_array.append([header, "\n"])
 
 
 
 
-    #open and repair each file
+#open and repair each file
 input_type = input("Enter input form ; file or directory(f/d) : ")
 check = input_type.lower()
 if check == "d":
@@ -53,7 +50,7 @@ if check == "d":
 
     #catch OS error    
     except(OSError):
-    print("Unable to find files in directory : {0}".format(path_to_files))
+        print("Unable to find files in directory : {0}".format(path_to_files))
 
     #open the file
     #try used to catch errors while opening the files
@@ -107,7 +104,7 @@ if check == "d":
         print("\n\nPermission to read {0} denied .".format(file))
         status = "Sorry, Conversion was unsuccessfull."
     
-
+    #catch name errors
     except(NameError):
         status = "Sorry, Conversion was unsuccessful."
         pass
@@ -121,66 +118,90 @@ if check == "d":
         
 
 if check == "f":
-    filename = input("Enter path to file (filename included) : ")
+    status = "Files converted successfully"
+    filepath = input("Enter path to file (filename included) : ")
+    path_to_outfiles = input("Enter destination directory (press return key for default) : ")
+    files = filepath.split("\\")[:-1]
+    Files = filepath.split("\\")
+    
+    #Declare default
+    if path_to_outfiles == "":
+        
+        path_to_outfiles = "" 
+        i=0
+
+        for i in range(len(files)):
+            path_to_outfiles =  files[-i] + "/" + path_to_outfiles
+        path_to_outfile = path_to_outfiles  
+        print(path_to_outfile)
+
+        #print(path_to_outfiles) 
+
 
     #to pick only html files
-    if  filename.endswith("html"):
+    if  filepath.endswith("html"):
+        
+        filename = Files[-1]
         try:  
             #print (filename)
-            infile = open(filename, "r")
-            print ("\n\nOpening {0} ...".format(file)  )
+            infile = open(filepath, "r")
+            print ("\n\nOpening {0} ...".format(filename)  )
             print("\nworking on file ...")
             text = infile.read()
+            #print (text)
             i = 0
             
             #substitute html characters in the file according to the substitution array
             for i in range(len(sub_array)): 
 
                 r = re.compile(sub_array[i][0])
-            # print  (sub_array[i][0])
+             #  print  (sub_array[i][0])
                 repl = str(sub_array[i][1])    
-            # print (repl)        
+            #   print (repl)        
                 outtext, count = re.subn(r,repl,text )
                 text = outtext
-            # print(outtext)
-            print("\n" + file + " successfully repaired !!!")
+            #    print(outtext)
+            print("\n" + filename + " successfully repaired !!!")
 
             #close file
             infile.close()
 
             #print output file
-            Check = input("print output ?(y/n)")
-            check = Check.lower()
-            if check == "y":     
-                print ("\n\nOutput for {0}: \n".format(file))
+            CHEck = input("print output ?(y/n)")
+            CHeck = CHEck.lower()
+            if CHeck == "y":     
+                print ("\n\nOutput for {0}: \n".format(filename))
                 print (outtext)
             
 
             #write substituted text to new file
-            outfile_name = "Repaired"+file+".txt"
-            outfile = path_to_outfiles+"/"+outfile_name
+            outfile_name = "Repaired"+ filename +".txt"
+            outfile = path_to_outfile + outfile_name
             Outfile = open(outfile, "w")
             Outfile.write(text)
             Outfile.close()
-        else:
-            print("Not a HTML file .")
 
-    #catch permission errors
-    except(PermissionError):
-        print("\n\nPermission to read {0} denied .".format(file))
-        status = "Sorry, Conversion was unsuccessfull."
-    
 
-    except(NameError):
-        status = "Sorry, Conversion was unsuccessful."
-        pass
+        #catch permission errors
+        except(PermissionError):
+            print("\n\nPermission to read {0} denied .".format(file))
+            status = "Sorry, Conversion was unsuccessfull."
         
 
-        
-    #finally ..
-    finally :
-        print("\n\n"+ status)
-        print("\n\nfor more amazing scripts : IG: iamcoder_o, Github: olusegvn")
+        except(NameError):
+            status = "Sorry, Conversion was unsuccessful."
+            pass
+
+        except(OSError):
+            status = "Sorry, Conversion was unsuccessful."
+            print ("OS Error occured, please try specifying file path : ")
+            
+
+            
+        #finally ..
+        finally :
+            print("\n\n"+ status)
+            print("\n\nfor more amazing scripts : IG: iamcoder_o, Github: olusegvn")
         
 
         
